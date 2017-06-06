e545414be171184b5b4d0854d8ef4eb87cb4077b
Anuswan91 <vivien.blum@free.com>
2017-06-06 13:39:12 +0200

    data base set up

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,6 @@
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
+    console.log(req.body)
     res.send('Hello')
   });
 };
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 
 const port = 8000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 require('./app/routes')(app, {});
 app.listen(port, () => {
   console.log('We are live on ' + port);
